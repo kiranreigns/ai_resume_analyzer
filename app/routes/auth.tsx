e8691282d6374ab86dfeadd1540ec639091c810a
Kiran <kiranreigns@hotmail.com>
2025-07-24 15:27:41 +0530

    implement Cloud storage, PDF-Image conversion, key-value storage and AI-feedback

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -21,18 +21,17 @@ const Auth = () => {
   }, [auth.isAuthenticated, next]);
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg">
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
             <h2>Log in to continue your job journey</h2>
           </div>
-
           <div>
             {isLoading ? (
               <button className="auth-button animate-pulse">
-                <p>Signing in...</p>
+                <p>Signing you in...</p>
               </button>
             ) : (
               <>
